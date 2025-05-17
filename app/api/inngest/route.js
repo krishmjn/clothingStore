5ad82fc8db4f0c73db641e20ccda00d7879b06ad
krishmjn <maharjankrish51@gmail.com>
2025-05-17 21:36:20 +0545

    fix

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -5,7 +5,6 @@ import {
   syncUserDeletion,
   syncUserUpdate,
 } from "@/config/inngest";
-import { helloWorld } from "../../../inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
